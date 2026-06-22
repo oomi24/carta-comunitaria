abffc34eea1aa07bc411c9e968ad4a22b8247cac
oomi24 <157886838+oomi24@users.noreply.github.com>
2026-06-22 14:24:35 +0000

    Código completo en la raíz y configurado

---
+++ android/app/src/main/kotlin/com/example/carta_comunitaria/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.carta_comunitaria
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app/lib/config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const String url = 'https://tu-proyecto.supabase.co';
+  static const String anonKey = 'tu-clave-anonima';
+}
